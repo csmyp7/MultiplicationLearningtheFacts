754a386de476006eb1aed415a30ed49f418e62e4
McCullough <csmyp7@mail.umkc.edu>
2018-02-19 06:57:40 -0600

    commit 2

---
+++ app/src/test/java/com/multiplication/mccullough/multiplicationlearningthefacts/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.multiplication.mccullough.multiplicationlearningthefacts
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
